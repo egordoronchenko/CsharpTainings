72960c2b94e3217a6f14671a354ead0fbd931b6e
Egor Doronchenko <massda@yandex.ru>
2023-07-04 22:57:22 +0300

    Задача 6 сделана

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -13,24 +13,39 @@
 //     Console.Write("Второе больше первого");
 // }
 
-// Задача 4
+// // Задача 4
 
-Console.WriteLine("Введите первое число");
-int Father = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int Son = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("ВВедите третье число");
-int HolySpirit = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int Father = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int Son = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("ВВедите третье число");
+// int HolySpirit = Convert.ToInt32(Console.ReadLine());
 
-int Trinity = Father;
+// int Trinity = Father;
 
-if (Trinity < Son)
+// if (Trinity < Son)
+// {
+//     Trinity = Son;
+// }
+// if (Trinity < HolySpirit)
+// {
+//      Trinity = HolySpirit;
+// }
+
+// Console.Write($"Наибольшее из введеных чисел это - {Trinity}");
+
+
+ //Задача 6
+
+ Console.WriteLine("Введи число");
+ int Numerus = Convert.ToInt32(Console.ReadLine());
+ int Veritas = Numerus%2;
+if (Veritas == 0)
 {
-    Trinity = Son;
+    Console.Write ($"Число {Numerus} четное");
 }
-if (Trinity < HolySpirit)
+else
 {
-     Trinity = HolySpirit;
+    Console.Write("Число "+Numerus+" нечетное");
 }
-
-Console.Write($"Наибольшее из введеных чисел это - {Trinity}");
