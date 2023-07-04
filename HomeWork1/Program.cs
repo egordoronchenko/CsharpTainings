f1623cfc10cc270c30cde5af0038eab00e6f901f
Egor Doronchenko <massda@yandex.ru>
2023-07-04 22:36:07 +0300

    Задача 4 сделана

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -12,3 +12,25 @@
 // {
 //     Console.Write("Второе больше первого");
 // }
+
+// Задача 4
+
+Console.WriteLine("Введите первое число");
+int Father = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int Son = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите третье число");
+int HolySpirit = Convert.ToInt32(Console.ReadLine());
+
+int Trinity = Father;
+
+if (Trinity < Son)
+{
+    Trinity = Son;
+}
+if (Trinity < HolySpirit)
+{
+     Trinity = HolySpirit;
+}
+
+Console.Write($"Наибольшее из введеных чисел это - {Trinity}");
