8ea58eac0469e9f3ec36aebfd8ecb38e96305be6
Egor Doronchenko <massda@yandex.ru>
2023-07-04 22:21:07 +0300

    Задача 2 готова

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //Задача 2
+// Console.Write("Введите первое число: ");
+// int MisterFirst = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int MisterSecond = Convert.ToInt32(Console.ReadLine());
+
+// if (MisterFirst > MisterSecond)
+// {
+//     Console.Write("Первое число больше");
+// }
+// else
+// {
+//     Console.Write("Второе больше первого");
+// }
