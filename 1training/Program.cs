9a7ebaed75a4a852cf57d714a71c32e63d53f226
Egor Doronchenko <massda@yandex.ru>
2023-07-04 21:58:58 +0300

    Конец первого семинара

--- 1training/Program.cs
+++ 1training/Program.cs
@@ -1,7 +1,39 @@
-﻿int a = 25; // int тока целые цисла
-double b = 36,6; // числа с запятой. но может быть и целым
-string c = "Hellp world" // строка
-bool d = true; //булево
+﻿//int a = 25; // int тока целые цисла
+//double b = 36.6; // числа с запятой. но может быть и целым
+//string c = "Hellp world"; // строка
+//bool d = true; //булево
 
-Console.Write("Hello, World!");
+//int number1 = 5;
+//int number2 = 7;
 
+
+//Console.Write($"Hello, World!+{number1} and {number2}");
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quad2 = num2 * num2;
+
+if (num1 == quad2)
+{
+     Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");   
+}
+*/
+
+// Console.Write("Input positivea number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// //Console.Write("Input a second number: ");
+// //int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+// curent = -number;
+
+// while(curent <= number)
+// {
+// Console.WriteLine
+
+// }
