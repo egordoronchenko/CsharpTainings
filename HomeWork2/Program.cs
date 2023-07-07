040d534de89f1cfe44c2956170d34910983ceb1f
Egor Doronchenko <massda@yandex.ru>
2023-07-07 23:31:36 +0300

    Задача 10 домашнего задания готова

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача1
+
+int SecondNumber(int num)
+{
+     int dozen = num/10;
+     int unit = dozen%10;
+     return unit;
+}
+Console.WriteLine("Введите 3х значное число");
+int third = Convert.ToInt32(Console.ReadLine());
+if (third < 100 ||third>999) 
+Console.WriteLine($"{third} не является 3-х значным числом ");
+
+int result = SecondNumber(third);
+Console.WriteLine(result);
