c463e40628073e911102fb41474d0f1b6889b7c3
Egor Doronchenko <massda@yandex.ru>
2023-07-07 19:57:41 +0300

    Корректировка кода в 1 тренировке

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,56 +1,56 @@
-﻿// //Задача 2
-// Console.Write("Введите первое число: ");
-// int MisterFirst = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите второе число: ");
-// int MisterSecond = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 2
+Console.Write("Введите первое число: ");
+int MisterFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int MisterSecond = Convert.ToInt32(Console.ReadLine());
 
-// if (MisterFirst > MisterSecond)
-// {
-//     Console.Write("Первое число больше");
-// }
-// else
-// {
-//     Console.Write("Второе больше первого");
-// }
+if (MisterFirst > MisterSecond)
+{
+    Console.Write("Первое число больше");
+}
+else
+{
+    Console.Write("Второе больше первого");
+}
 
-// // Задача 4
+// Задача 4
 
-// Console.WriteLine("Введите первое число");
-// int Father = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int Son = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("ВВедите третье число");
-// int HolySpirit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int Father = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int Son = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите третье число");
+int HolySpirit = Convert.ToInt32(Console.ReadLine());
 
-// int Trinity = Father;
+int Trinity = Father;
 
-// if (Trinity < Son)
-// {
-//     Trinity = Son;
-// }
-// if (Trinity < HolySpirit)
-// {
-//      Trinity = HolySpirit;
-// }
+if (Trinity < Son)
+{
+    Trinity = Son;
+}
+if (Trinity < HolySpirit)
+{
+     Trinity = HolySpirit;
+}
 
-// Console.Write($"Наибольшее из введеных чисел это - {Trinity}");
+Console.Write($"Наибольшее из введеных чисел это - {Trinity}");
 
 
- //Задача 6
+//  Задача 6
 
-//  Console.WriteLine("Введи число");
-//  int Numerus = Convert.ToInt32(Console.ReadLine());
-//  int Veritas = Numerus%2;
-// if (Veritas == 0)
-// {
-//     Console.Write ($"Число {Numerus} четное");
-// }
-// else
-// {
-//     Console.Write("Число "+Numerus+" нечетное");
-// }
+ Console.WriteLine("Введи число");
+ int Numerus = Convert.ToInt32(Console.ReadLine());
+ int Veritas = Numerus%2;
+if (Veritas == 0)
+{
+    Console.Write ($"Число {Numerus} четное");
+}
+else
+{
+    Console.Write("Число "+Numerus+" нечетное");
+}
 
-//Задача 8
+// Задача 8
 
 Console.WriteLine("Введи число");
 int number = Convert.ToInt32(Console.ReadLine());
