b07cc7da59784a6e2a65ed24576b37cf9235948e
Egor Doronchenko <massda@yandex.ru>
2023-07-08 00:03:17 +0300

    Вторая задача домашнего задания выполнена

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача1
-
+/*
 int SecondNumber(int num)
 {
      int dozen = num/10;
@@ -13,3 +13,31 @@ Console.WriteLine($"{third} не является 3-х значным число
 
 int result = SecondNumber(third);
 Console.WriteLine(result);
+*/
+
+//Задача2 
+
+int SempleSolution(int num)
+{
+    while (num >999)
+    {
+        num = num/10; 
+    }
+int result = num%10;
+return result;    
+
+}
+
+
+Console.WriteLine("Введите число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if (threeDigitNumber <100) 
+{
+    goto Found;
+}
+
+
+Console.WriteLine(SempleSolution(threeDigitNumber));
+Found:
+    Console.WriteLine($"число {threeDigitNumber} двузначное. Третьей цифры нет");
