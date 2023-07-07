dced8cef71563b3312f3674e24c06b5803f29f68
Egor Doronchenko <massda@yandex.ru>
2023-07-07 20:49:56 +0300

    первая задачка

--- 2training/Program.cs
+++ 2training/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит 
+// случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+
+//Задача1
+int CutNumber(int num)
+{
+
+    int hundreds = num/100;
+    int unit =  num%10;
+
+    int result = hundreds*10 + unit;
+    return result;
+    }
+
+int randomNumber = new Random().Next(100,1000);
+
+int newNumber = CutNumber(randomNumber);
+Console.WriteLine($"New version of {randomNumber} is {newNumber}");
+
+
