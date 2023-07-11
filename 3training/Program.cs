90bc92ed0fb02ac7329322e6ee0ecf3732357551
Egor Doronchenko <massda@yandex.ru>
2023-07-11 20:34:04 +0300

    первая задача готова

--- 3training/Program.cs
+++ 3training/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+void ShowDiapazone (int quad)
+{
+    if (quad == 1) Console.WriteLine("x>0 and y>0");
+    else if(quad == 2) Console.WriteLine("x<0 and y>0");
+    else if(quad == 3) Console.WriteLine("x<0 and y<0");
+    else if(quad == 4) Console.WriteLine("x>0 and y<0");
+    else Console.WriteLine("Wrong input");
+}
+
+Console.WriteLine("Input a number of quadrant (from 1 to 4)");
+int quadrant = Convert.ToInt32(Console.ReadLine());
+
+ShowDiapazone(quadrant);
+*/
