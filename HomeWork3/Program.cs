b1a3a0eb96ca77792674face1d1457451c20f7f7
Egor Doronchenko <massda@yandex.ru>
2023-07-16 15:40:58 +0300

    первая задача

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-sdfsdfsd
+﻿
+
+int t, r;
+int sum = 0;
+bool IsPalindrom(int number)
+    { for(t=number;number!=0;number=number/10)
+        {
+        r=number % 10;
+        sum=sum*10+r;
+        }
+            if(t==sum)
+            return true;
+            else
+            return false;
+    } 
+
+Console.WriteLine("Input a number");
+link1:
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number <100000  )
+    {bool result = IsPalindrom(number);
+        if (result == true)
+        Console.WriteLine("Палиндром");
+        else Console.WriteLine("Число не палиндром");
+     }
+else
+    {Console.WriteLine("число не пятизначное. Введите новое число");
+    goto link1;
+    }
+
+
+
