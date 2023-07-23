dd05df842ca0de0ac0a1ccd6706f7df4d9859d46
Egor Doronchenko <massda@yandex.ru>
2023-07-23 12:50:19 +0300

    обновляю всю папку

---
+++ 5training/Program.cs
@@ -0,0 +1,95 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из 
+//промежутка [-9, 9]. Найдите сумму отрицательных элементов массива.
+/*cl
+int sum;
+int ReadInt (string message)
+    {
+        Console.WriteLine(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+int[] CreateRandomArrow(int size,int minValue,int maxValue)
+
+{
+    int[] array = new int[size];
+        for(int i = 0; i < array.Length;i++)
+        {
+            array[i] = new Random().Next(minValue, maxValue+1);
+        }
+        return array;
+}
+
+int NegativeSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0;i < array.Length;i++) 
+        if (array[i] < 0)
+        {
+         sum += array[i];
+        }
+        return sum;
+}
+int size = ReadInt("введите ");
+int minValue = ReadInt("ddtlbnt -9");
+int maxValue = ReadInt("введите 9");
+int[] myArray =CreateRandomArrow(size, minValue, maxValue);
+sum = NegativeSum(myArray);
+Console.WriteLine(sum);
+*/
+
+//Напишите программу замена элементов
+// массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+int ReadInt (string message)
+    {
+        Console.WriteLine(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+int[] CreateRandomArrow(int size,int minValue,int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] =new Random().Next(minValue,maxValue);
+        Console.WriteLine(array[i]);
+    }
+return array;
+
+}
+void ChangeArray(int[] array)
+   {
+     for(int i = 0; i < array.Length;i++)
+    {
+        array[i]= -1*array[i];
+        Console.WriteLine(array[i]);
+    }    
+   }
+int size = ReadInt("Введите размер массива");
+int minValue = ReadInt("Введите минимальное отрицательно число");   
+int maxValue = ReadInt("Введите максимальное число в массиве");
+
+int[]myArray = CreateRandomArrow(size, minValue, maxValue);
+ChangeArray(myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Задайте массив. 
+//Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//Задайте одномерный массив из m случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [a,b].
