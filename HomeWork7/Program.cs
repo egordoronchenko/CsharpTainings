07253ead8b690474d30ca5b6fb07519b3c1d7ee0
Egor Doronchenko <massda@yandex.ru>
2023-07-29 18:31:59 +0300

    HW7 done

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -58,3 +58,91 @@ ShowArray(myArray);
 8 4 2 4
 1 7 -> числа с такими индексами в массиве нет
 */
+/*
+int ReadInt(string messege)
+{
+    Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateArray(int rows, int columns)
+{
+    int [,] array = new int[rows, columns];
+    for(int i = 0; i < rows;i++)
+    {
+        for (int j = 0;j < columns;j++)
+        {
+            array[i,j] = new Random().Next(0, 255);
+        }
+    }
+    return array;
+}
+
+void CheckArray(int[,] array, int x, int y)
+{
+    if(x < 0 && y <0)  Console.WriteLine("Цифры должны быть больше 0");   
+    else 
+    {
+        if(x < array.GetLength(0) && y < array.GetLength(1)) 
+        Console.WriteLine($"Значение ячейки {array[x,y]}");
+        else  Console.WriteLine("Такой ячейки не существует в массиве");
+    }
+}
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество столбцов");   
+int [,] myArray = CreateArray(rows, columns);
+int x = ReadInt("Введите строку поиска");
+int y = ReadInt("Введите столбец поиска");
+CheckArray(myArray, x, y);
+*/
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+int ReadInt(string messege)
+{
+    Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < rows;i++)
+    {
+        for(int j = 0;j < columns;j++)
+        {
+        array[i,j] = new Random().Next(1,11);
+        }
+    }
+    return array;
+}
+
+void SumColumns(int[,]array)
+{
+   
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0.0;
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i,j];
+        }
+        double result = Math.Round(sum / array.GetLength(0),2);
+       Console.Write(result+", ");
+    }
+}
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество столбцов");
+int[,] myArray = CreateArray(rows, columns);
+SumColumns(myArray);
